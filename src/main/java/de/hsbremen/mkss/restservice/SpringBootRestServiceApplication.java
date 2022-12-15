688c588fe916631a840b092f0a400c25fc35f0d2
NLenwa <34138716+NLenwa@users.noreply.github.com>
2022-12-15 15:26:11 +0100

    MKSS task 6 spring

---
+++ src/main/java/de/hsbremen/mkss/restservice/SpringBootRestServiceApplication.java
@@ -0,0 +1,16 @@
+package de.hsbremen.mkss.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiceApplication.class, args);
+	}
+
+}
+
+
+
